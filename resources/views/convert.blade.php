209e56679a8c5cf12f48b7cd1ea3e6757fe62f5b
Khien <kcp@cct-technology.com>
2023-11-24 08:57:49 +0700

    .

--- resources/views/convert.blade.php
+++ resources/views/convert.blade.php
@@ -1,14 +1,16 @@
 @extends('layout')
 
 @section('content')
-    <form action="/bang-diem/save" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="formFile" class="form-label">Upload one .zip/.xls file</label>
-            <input class="form-control" type="file" id="formFile" name="file" required accept=".zip, .xls">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="row">
+        <form action="/bang-diem/save" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Upload one .zip/.xls file</label>
+                <input class="form-control" type="file" id="formFile" name="file" required accept=".zip, .xls">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 
     <hr>
     <div class="row">
@@ -21,11 +23,9 @@
     <div class="row">
         <form action="/bang-diem/code/check" method="POST">
             @csrf
-            <div class="mb-12">
-                <label for="formFile" class="form-label">Danh sách mã LHP cần kiểm tra</label>
-                <label>
-                    <textarea class="form-control" name="codes" rows="10"></textarea>
-                </label>
+            <div class="mb-3">
+                <label for="list_code" class="form-label">Danh sách mã LHP cần kiểm tra</label>
+                <textarea class="form-control" id="list_code" name="codes" rows="10" placeholder="code1&#10;code2"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
